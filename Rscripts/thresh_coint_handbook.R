7153c5ac7bd9e30c2f386f0bf7415fe0046377a8
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-01-09 20:00:34 +0100

    add as .data fram ur ers

--- Rscripts/thresh_coint_handbook.R
+++ Rscripts/thresh_coint_handbook.R
@@ -1,1 +1,15 @@
 library(tidyverse)
+
+
+## convert a ur.ers test output to data frame
+as.data.frame.ur.ers <- function(x, ...) {
+  res <- cbind(type = x@type, 
+               model = x@model, 
+               teststat = x@teststat,
+               as.data.frame(x@cval),
+               lag = x@lag,
+               stringsAsFactors = FALSE)
+  res <- as.data.frame(res)
+  rownames(res) <-  NULL
+  res
+}
