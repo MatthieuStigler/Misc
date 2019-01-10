5e839230c10dd72296cf3fa6c0ded70020ee7850
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-01-10 13:54:06 +0100

    Add link to other script

--- Rscripts/thresh_coint_handbook.R
+++ Rscripts/thresh_coint_handbook.R
@@ -1,5 +1,6 @@
 library(tidyverse)
-
+library(devtools)
+source_url("https://raw.githubusercontent.com/MatthieuStigler/Misconometrics/master/irf_for_ggplot/as.data.frame.varirf.R")
 
 ## convert a ur.ers test output to data frame
 as.data.frame.ur.ers <- function(x, ...) {
