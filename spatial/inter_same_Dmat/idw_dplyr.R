f7cb891955e773df99f85815a471cd299ac721ea
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-04-23 15:42:55 -0700

    Add files idw_dplyr

---
+++ spatial/inter_same_Dmat/idw_dplyr.R
@@ -0,0 +1,331 @@
+library(gstat)
+library(sp)
+library(sf)
+library(tidyverse)
+
+
+
+## #### gstat classic
+idw0_W = function (data, newdata, y, idp = 2) {
+  s = coordinates(data)
+  s0 = coordinates(newdata)
+  D = spDists(s0, s)
+  D
+} 
+
+dist_mat_any <- function(data_1, data_2) {
+  if(inherits(data_1, c("SpatialPointsDataFrame", "SpatialPixelsDataFrame"))) {
+    dists <- spDists(coordinates(data_2), coordinates(data_1))
+  } else {
+    dists <- st_distance(data_2, data_1) 
+  }
+  dists 
+}
+
+idw_getW <- function (data, newdata, idp = 2, maxdist=Inf, nmin=0, nmax=Inf, 
+                      D=NULL, normalize=TRUE, force = FALSE) {
+
+  if(is.null(D)) D <- dist_mat_any(data, newdata)
+  if (inherits(D, "units")) D <- drop_units(D)
+  # D_has_zero <- any(D==0)
+  # D_n_zero <- sum(D==0) 
+  
+  W = 1/(D^idp)
+  if(!force | (force & nmin==0 & !is.finite(maxdist))) {
+    if(is.finite(maxdist)) W[D>=maxdist] <- 0  
+    
+    ## nmin: set to w=0 all rows which don't have at least nmin non-zero values
+    if(nmin>0) {
+      below_max <- rowSums(W!=0)
+      W[below_max<nmin,] <- 0
+    }
+  } else {
+    order = t(apply(D, 1, function(x) rank(x, ties.method = "first")))
+    W[order > nmin & D>=maxdist] <- 0
+  }
+  
+  if(is.finite(nmax)) {
+    order = t(apply(D, 1, function(x) rank(x, ties.method = "first")))
+    # order[1:5,1:5]
+    # W[1:5,1:5]
+    W[order >= nmax+1] <- 0
+  }
+  
+  
+  
+  if(normalize){
+    # sumD = apply(W, 1, function(x) sum(x, na.rm=TRUE))
+    sumD = rowSums(W, na.rm=TRUE)
+    res <- W/sumD
+  } else {
+    res <- W
+  }
+  res[is.nan(res)] <- NA
+  res
+} 
+
+
+#### Parallel version
+# idw_getW_para <- function(data, newdata, idp = 2, maxdist=Inf, nmin=0, nmax=Inf, 
+#                            D=NULL, normalize=TRUE, force = FALSE, cl) {
+#   ## Prepare y
+#   if(inherits(data, "sf")) {
+#     data_df <- data %>%
+#       st_set_geometry(NULL) %>%
+#       as_data_frame
+#   } else {
+#     data_df <- data
+#   }
+#   y_df <- data_df %>%
+#     select_if(is.numeric)
+#   Y <- as.matrix(y_df)
+#   
+#   ## D
+#   if(is.null(D)) D <- dist_mat_any(data, newdata)
+#   
+#   ## split y
+#   y_groups <- clusterSplit(cl, seq_len(ncol(Y)) )  #split(n_Y, sort(n_Y%%2))
+#   Y_li <- lapply(y_groups, function(i) Y[, i])
+#   lapply(Y_li, dim)
+#   ## run
+#   outp_par <- clusterApplyLB(cl, Y_li, function(x) idw_getW(x, newdata, idp = idp, 
+#                                                                     maxdist = maxdist, nmin = nmin, nmax = nmax, 
+#                                                                     D = D, normalize = normalize, force = force))
+#   outp_par
+# }
+  
+
+idw_W_y <- function (W, y) {
+  W %*% y
+}
+
+idw_do <- function(data, newdata, idp = 2, maxdist=Inf, nmin=0, nmax=Inf, D=NULL, force=FALSE, y) {
+  W <- idw_getW(data=data, newdata=newdata, idp = idp, maxdist=maxdist, nmin=nmin, nmax=nmax, D=D, force=force)
+  W %*% y
+}
+
+idw_tidy <- function(data, newdata, idp = 2, maxdist=Inf, nmin=0, nmax=Inf, D=NULL,
+                     na.rm=TRUE, add_name = "pred", force=FALSE,
+                     parallel = NULL) {
+  
+  
+  if(!is.null(parallel)) {
+    if(!inherits(parallel, "cluster")) stop("'parallel' should be a 'cluster' object")
+    require(parallel)
+    use_parallel <- TRUE
+  } else {
+    use_parallel <- FALSE
+  }
+  
+  
+  ## select y
+  if(inherits(data, "sf")) {
+    data_df <- data %>%
+      st_set_geometry(NULL) %>%
+      as_data_frame
+  } else {
+    data_df <- data
+  }
+  y_df <- data_df %>%
+    select_if(is.numeric)
+  Y <- as.matrix(y_df)
+  
+  ## Compute D
+  if(is.null(D)) {
+    D <- st_distance(newdata, data)
+  } else {
+    if(ncol(D)!=nrow(Y) | nrow(D)!=nrow(newdata)) stop("Issues in dim of D?")
+  }
+  
+  ## NA cases
+  if(na.rm) {
+    # Y_na <- is.na(Y) 
+    # has_NA <- any(Y_na)
+    has_NA <- anyNA(Y)
+  }
+  normalize <- ifelse(na.rm && has_NA, FALSE, TRUE)
+  
+  
+  ## Get weights
+
+  
+  
+  ## NA handling: if NA, set W and Y to 0, renormalize
+  if(na.rm && has_NA) {
+    no_na_rm <- function(y) {
+      y_na <- is.na(y)
+      if(TRUE) {
+        y[y_na] <- 0 #Actually not really necessary, more for stability
+        D[,y_na] <- Inf # will result in weight of zero
+        W_i <- idw_getW(#data=data, newdata=newdata,  actually won't be used as D provided
+          idp = idp, maxdist=maxdist, nmin=nmin, nmax=nmax, 
+          force=force,
+          normalize=TRUE,
+          D=D)
+        as.numeric(W_i %*% y)
+      } else {
+        y_nona <- y[!y_na]
+        D_nona <- D[, !y_na]
+        
+        W_i <- idw_getW(#data=data, newdata=newdata,  actually won't be used as D provided
+          idp = idp, maxdist=maxdist, nmin=nmin, nmax=nmax, 
+          force=force,
+          normalize=TRUE,
+          D=D_nona)
+        as.numeric(W_i %*% y_nona)  
+      }
+      
+      
+    }
+    
+    Y_li <- lapply(seq_len(ncol(Y)), function(i) Y[, i])
+    if(use_parallel) {
+      on.exit(stopCluster(parallel))
+      res_li <- clusterApplyLB(parallel, Y_li, no_na_rm)
+      
+    } else {
+      res_li <- lapply(Y_li, no_na_rm)  
+    }
+    
+    res <- bind_cols(res_li)
+    colnames(res) <- colnames(Y)
+    
+  } else {
+    
+    if(use_parallel) {
+      y_groups <- clusterSplit(cl, seq_len(nrow(newdata)) )  #split(n_Y, sort(n_Y%%2))
+      clusterExport(cl=cl, list("data", "D", "newdata", "idp", "force", "nmax", "Y",
+                                "nmin", "maxdist", "normalize")) #                    envir=environment())
+      on.exit(stopCluster(parallel))
+      outp_par <- clusterApplyLB(parallel, y_groups, 
+                                   function(i) idw_getW(data=data, newdata=newdata[i,], 
+                                                             idp = idp, maxdist=maxdist, nmin=nmin, nmax=nmax, 
+                                                             force= force,
+                                                             normalize=normalize,
+                                                             D=D[i, ]) %*% Y)
+      res <- do.call("rbind", outp_par) %>% as_data_frame
+    } else {
+      W <- idw_getW(data=data, newdata=newdata, 
+                    idp = idp, maxdist=maxdist, nmin=nmin, nmax=nmax, 
+                    force= force,
+                    normalize=normalize,
+                    D=D)
+      res <- as_data_frame(W %*% Y)
+    }
+  }
+  if(!is.null(add_name)) {
+    colnames(res) <- paste(colnames(res), add_name, sep="_")
+  }
+  
+  ## get outputs
+  
+  # final <- sf:::cbind.sf(newdata, res)
+  if(nrow(newdata) == nrow(res)) {
+    final <- bind_cols(newdata, res)  
+  } else {
+    warning(paste("nrow newdata (", nrow(newdata), ") not equal to nrow(res)", nrow(res)))
+    final <- res
+  }
+  
+  final
+}
+
+idw0_mat <- function(data, newdata, idp = 2, maxdist=Inf, nmin=0, nmax=Inf, D=NULL, force=FALSE, y) {
+  res <- idw_do(data=data, newdata=newdata, idp = idp, maxdist=maxdist, nmin=nmin, nmax=nmax, D=D, y=y, force=force)
+  data.frame(value=res) %>%
+    as_tibble
+}   
+
+# idw0_mat <- function (data, newdata, y, idp = 2, maxdist=Inf, nmin=0, nmax=Inf) {
+#   s = coordinates(data)
+#   s0 = coordinates(newdata)
+#   W = spDists(s0, s)
+#   D = 1/(W^idp)
+#   if(is.finite(maxdist)) D[W>maxdist] <- 0
+#   if(is.finite(nmax)) {
+#     # order = t(apply(W, 1, function(x) sort.int(x, index.return=TRUE)$ix))
+#     order = t(apply(W, 1, function(x) rank(x, ties.method = "first")))
+#     order[1:5,1:5]
+#     W[1:5,1:5]
+#     D[order >= nmax+1] <- 0
+#   }
+#   if(nmin>0) {
+#     below_max <- rowSums(D!=0)
+#     D[below_max<nmin,] <- 0
+#   }
+#   sumD = apply(D, 1, function(x) sum(x, na.rm=TRUE))
+#   # data.frame(weight= D, value=D %*% y/sumD)
+#   res <- D %*% y/sumD
+#   res[is.nan(res)] <- NA
+#   data.frame(value=res) %>% as_tibble
+# } 
+
+##
+# dist_mat_sf <- function(data1, data2) {
+#   st_distance(st_as_sf(meuse.grid), st_as_sf(meuse)) %>%
+#     as_data_frame() %>%
+#     mutate(index= 1:n()) %>%
+#     select(index, everything())
+# }
+
+
+dist_mat_sf <- function(data_1, data_2) {
+  if(inherits(data_1, c("SpatialPointsDataFrame", "SpatialPixelsDataFrame"))) {
+    dists <- spDists(coordinates(data_2), coordinates(data_1))
+  } else {
+    dists <- st_distance(data_2, data_1) 
+  }
+  dists %>%
+    as_data_frame() %>%
+    mutate(index= 1:n()) %>%
+    select(index, everything())
+}
+
+
+
+idw_tidy_way <- function(data, newdata, idp=2, nmax=Inf, nmin=0, maxdist=Inf){
+  D_df <- dist_mat_sf(data_1=data, data_2=newdata)
+
+  ## long
+  D_df_l <- D_df %>%
+    gather(obs, distance, -index) %>%
+    left_join(mutate(as_data_frame(meuse), obs=paste("V", 1:n(), sep="")), by="obs" )
+
+  if(!is.infinite(nmax)) {
+    D_df_l <- D_df_l %>%
+      arrange(index, distance) %>%
+      group_by(index) %>%
+      slice(1:nmax)
+  }
+
+  if(!is.infinite(maxdist)) {
+    D_df_l <- D_df_l %>%
+      filter(distance <=maxdist)
+  }
+
+
+  if(nmin >0) {
+    D_df_l <- D_df_l %>%
+      add_count(index) %>%
+      filter(n>=nmin) %>%
+      select(-n)
+  }
+
+  ## aggreg
+  aggreg <- D_df_l %>%
+    group_by(index) %>%
+    summarise(mean = weighted.mean(zinc, w= 1/(distance^2))) %>%
+    ungroup() %>%
+    complete(index= D_df$index)
+
+  aggreg
+}
+
+# 
+# 
+# 
+# #
+# # rank(c(3, 2, 1))
+# # rank(c(3, 2, 1, 1), ties.method = "min")
+# # rank(c(3, 2, 1, 1, 1))
+# # rank(c(3, 2, 1, 1, 1, 1))
---
+++ spatial/inter_same_Dmat/idw_dplyr_USE.R
@@ -0,0 +1,270 @@
+
+library(gstat)
+library(sp)
+library(sf)
+
+library(tidyverse)
+
+
+source("/home/matifou/Dropbox/GIS_server/r_scripts_gis_general/idw_dplyr.R")
+
+
+################
+## DATA
+################
+
+data(meuse)
+coordinates(meuse) = ~x+y
+data(meuse.grid)
+gridded(meuse.grid) = ~x+y
+
+
+meuse_sf <- st_as_sf(meuse)
+meuse.grid_sf <- st_as_sf(meuse.grid)
+
+D_sp <- dist_mat_sf(data_1=meuse, meuse.grid)
+D_sf <- dist_mat_sf(meuse_sf, meuse.grid_sf)
+
+all.equal(D_sp, D_sf)
+
+
+## explanation: 
+##    -data: we have 155 observed obs (meuse: 155 x 12), 
+##    -newdata: want on 3103 points
+##    -D: 3103 x 155, i.e. newpoint to old point
+##    -W: weights from D 
+##    -W %*% newdata: (3103 x 155) x (155 x k) gives 3130 x k new data
+
+##    -if y missing at j: set full j column to Inf distance => W has 0
+
+
+################
+## RUNS
+################
+meuse.gstat <- gstat(id = "zinc", formula = zinc ~ 1, data = meuse, set = list(idp = 2))
+
+meuse.gstat2 <- gstat(id = "zinc", formula = zinc ~ 1, data = meuse, 
+                      maxdist = 205, set = list(idp = 2))
+meuse.gstat3 <- gstat(id = "zinc", formula = zinc ~ 1, data = meuse, 
+                      nmax=3 , set = list(idp = 2))
+
+meuse.gstat4 <- gstat(id = "zinc", formula = zinc ~ 1, data = meuse, 
+                      nmin=3, maxdist=205, set = list(idp = 2))
+meuse.gstat5 <- gstat(id = "zinc", formula = zinc ~ 1, data = meuse, 
+                      nmin=3, maxdist=205, force=TRUE, set = list(idp = 2))
+
+
+gs_1 <- predict(meuse.gstat, meuse.grid) %>%  as_data_frame()
+gs_2 <- predict(meuse.gstat2, meuse.grid) %>%  as_data_frame()
+gs_3 <- predict(meuse.gstat3, meuse.grid) %>%  as_data_frame()
+gs_4 <- predict(meuse.gstat4, meuse.grid) %>%  as_data_frame()
+gs_5 <- predict(meuse.gstat5, meuse.grid) %>%  as_data_frame()
+
+id_man1 <- idw0_mat(data = meuse, newdata=meuse.grid, y= meuse$zinc)
+id_man2 <- idw0_mat(data = meuse, newdata=meuse.grid, y= meuse$zinc, maxdist=205)
+id_man3 <- idw0_mat(data = meuse, newdata=meuse.grid, y= meuse$zinc, nmax=3) 
+id_man4 <- idw0_mat(data = meuse, newdata=meuse.grid, y= meuse$zinc, maxdist=205, nmin=3)
+id_man5 <- idw0_mat(data = meuse, newdata=meuse.grid, y= meuse$zinc, maxdist=205, nmin=3, force=TRUE)
+
+idt_1 <- idw_tidy_way(data = meuse, newdata=meuse.grid)
+idt_2 <- idw_tidy_way(data = meuse, newdata=meuse.grid, maxdist=205) 
+idt_3 <- idw_tidy_way(data = meuse, newdata=meuse.grid, nmax=3)
+idt_4 <- idw_tidy_way(data = meuse, newdata=meuse.grid, maxdist=205, nmin=3) 
+
+
+id_fin_1 <- idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf)
+id_fin_2 <- idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, maxdist=205)
+id_fin_3 <- idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, nmax=3)
+id_fin_4 <- idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, maxdist=205, nmin=3)
+id_fin_5 <- idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, maxdist=205, nmin=3, force=TRUE)
+
+
+id_fin_multi_1 <- idw_tidy(data = meuse_sf, newdata=meuse.grid_sf)
+id_fin_multi_2 <- idw_tidy(data = meuse_sf, newdata=meuse.grid_sf, maxdist=205)
+id_fin_multi_3 <- idw_tidy(data = meuse_sf, newdata=meuse.grid_sf, nmax=3)
+id_fin_multi_4 <- idw_tidy(data = meuse_sf, newdata=meuse.grid_sf, maxdist=205, nmin=3)
+id_fin_multi_5 <- idw_tidy(data = meuse_sf, newdata=meuse.grid_sf, maxdist=205, nmin=3, force=TRUE)
+
+##########################
+## Check
+##########################
+
+
+## Compare
+
+## standard case
+all.equal(idt_1$mean, gs_1$zinc.pred)
+all.equal(idt_1$mean, id_man1$value) 
+all.equal(idt_1$mean, id_fin_1$zinc_pred) 
+all.equal(idt_1$mean, id_fin_multi_1$zinc_pred) 
+
+## maxdist=205
+all.equal(idt_2$mean, gs_2$zinc.pred)
+all.equal(idt_2$mean, id_man2$value) 
+all.equal(idt_2$mean, id_fin_2$zinc_pred) 
+all.equal(idt_2$mean, id_fin_multi_2$zinc_pred) 
+
+## nmax =3
+all.equal(id_man3$value, gs_3$zinc.pred)
+all.equal(idt_3$mean, gs_3$zinc.pred)
+all.equal(idt_3$mean, id_man3$value) 
+all.equal(idt_3$mean, id_fin_3$zinc_pred) 
+all.equal(idt_3$mean, id_fin_multi_3$zinc_pred) 
+
+# nmin=3
+all.equal(idt_4$mean, gs_4$zinc.pred)
+all.equal(idt_4$mean, id_man4$value) 
+all.equal(idt_4$mean, id_fin_4$zinc_pred) 
+all.equal(idt_4$mean, id_fin_multi_4$zinc_pred) 
+
+
+## nmin AND maxdist
+all.equal(id_man5$value, gs_5$zinc.pred)
+all.equal(id_man5$value, id_fin_5$zinc_pred) 
+all.equal(id_man5$value, id_fin_multi_5$zinc_pred) 
+
+##########################
+## Multiple y
+##########################
+
+meuse.grid_sf2 <- meuse.grid_sf %>% select(geometry)
+meuse_sf_4_df <- meuse_sf_4 %>%
+  st_set_geometry(NULL) %>%
+  as_data_frame()
+
+Y <- as.data.frame(meuse)[c("cadmium", "copper", "lead", "zinc")]
+Y_mat <- as.matrix(Y)
+
+meuse_sf_4 <- meuse %>%
+  st_as_sf() %>%
+  select(cadmium, copper, lead, zinc)
+
+id_math_Y <- idw_tidy(data = meuse_sf_4, newdata = meuse.grid_sf2)
+id_math_Y
+dim(id_math_Y)
+
+
+plot(select(id_math_Y, cadmium_pred), reset = FALSE)
+plot(select(meuse_sf, cadmium), add=TRUE)
+
+### gstat way
+
+meuse.gstat <- gstat(id = "zinc", formula = zinc ~ 1, data = meuse, 
+                     nmax = 7, set = list(idp = .5))
+meuse.gstat
+z <- predict(meuse.gstat, meuse.grid)
+spplot(z["zinc.pred"])
+
+##########################
+## Parallel
+##########################
+
+
+dim(meuse_sf_4)
+dim(meuse.grid_sf2)
+
+
+## simple call get Weights
+D_all <- st_distance(meuse.grid_sf2, meuse_sf_4)
+D_1 <- st_distance(meuse.grid_sf2[1:1500,], meuse_sf_4)
+D_2 <- st_distance(meuse.grid_sf2[1501:3103,], meuse_sf_4)
+dim(D_all)
+dim(D_1)
+dim(D_2)
+all.equal(D_all, rbind(D_1, D_2))
+
+res_all <- idw_getW(data = meuse_sf_4, newdata=meuse.grid_sf2, D=D_all)
+res_1 <- idw_getW(data = meuse_sf_4, newdata=meuse.grid_sf2[1:1500,], D=D_all[1:1500,])
+res_2 <- idw_getW(data = meuse_sf_4, newdata=meuse.grid_sf2[1501:3103,], D= D_all[1501:3103,])
+
+dim(res_all)
+dim(rbind(res_1, res_2))
+
+all.equal(res_all, rbind(res_1, res_2))
+
+
+## now bind
+library(magrittr)
+res_bind <- rbind(as_data_frame(res_1 %*% as.matrix(meuse_sf_4_df)),
+                  as_data_frame(res_2 %*% as.matrix(meuse_sf_4_df)))
+
+id_math_Y <- idw_tidy(data = meuse_sf_4, newdata=meuse.grid_sf %>% select(geometry)) %>% 
+  st_set_geometry(NULL) %>%
+  as_data_frame() %>%
+  set_colnames(str_remove(colnames(.), "_pred"))
+
+## is same? yes!!
+all.equal(id_math_Y , res_bind)
+
+
+## para call low
+
+##
+id_math_Y <- idw_tidy(data = meuse_sf_4, newdata=meuse.grid_sf %>% select(geometry))
+dim(id_math_Y)
+
+##
+library(parallel)
+cl <- makeForkCluster(2)
+
+id_math_Y_par <- idw_tidy(data = meuse_sf_4, newdata=meuse.grid_sf %>% select(geometry),
+                      parallel = cl)
+
+
+all.equal(id_math_Y, id_math_Y_par)
+
+library(microbenchmark)
+
+microbenchmark(nopar  = idw_tidy(data = meuse_sf_4, newdata=meuse.grid_sf %>% select(geometry)),
+               par_2  = idw_tidy(data = meuse_sf_4, newdata=meuse.grid_sf %>% select(geometry), parallel = makeForkCluster(2)),
+               par_4  = idw_tidy(data = meuse_sf_4, newdata=meuse.grid_sf %>% select(geometry), parallel = makeForkCluster(4)),
+               times = 3)
+
+##########################
+## NAs case
+##########################
+
+meuse_sf_NA <- meuse_sf %>%
+  mutate(n_row = 1:n(),
+         lime2=ifelse(n_row %in% 1:3, NA, lime))
+
+## gstat way
+gs_out_NA <- meuse.gstat4 <- gstat(id = "lime2", formula = lime2 ~ 1, data = as(meuse_sf_NA, "Spatial"), 
+                                   set = list(idp = 2)) %>%
+  predict(meuse.grid) %>%
+  as_data_frame()
+
+out_NA <- idw_tidy(data = select(meuse_sf_NA,lime2), newdata=meuse.grid_sf)
+out_NA
+
+
+##########################
+## Profile
+##########################
+library(profvis)
+library(microbenchmark)
+
+D <- st_distance(meuse.grid_sf, select(meuse_sf, zinc))
+microbenchmark(a = idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, D=D), 
+               times = 50)
+
+
+
+
+profvis(idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, D=D), interval = 0.005)
+
+profvis(idw_tidy(data = meuse_sf, newdata=meuse.grid_sf, D=D), interval = 0.005)
+
+
+profvis(idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, D=D))
+
+
+##
+W <- idw_getW(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, D=D)
+W %*% as.matrix(meuse_sf$zinc) %>%
+  as_data_frame()
+
+idw_tidy(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, D=D) %>% select(zinc_pred)
+
+
+profvis(idw_getW(data = select(meuse_sf, zinc), newdata=meuse.grid_sf, D=D), interval = 0.005)
